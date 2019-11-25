a4a75259f80682c93a1aec7df54cf9bf183e38d1
Jason Santiago <jasonsantiagobutler@gmail.com>
2019-11-25 19:58:16 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
   const configService = app.get(ConfigService)
-  await app.listen(process.env.PORT || configService.get("PORT") || 3000)
+  await app.listen(process.env.PORT || configService.get("PORT") || 7000)
 }
 bootstrap();
