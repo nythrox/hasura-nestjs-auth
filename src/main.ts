1c40a5665fb2d4bbc5898064619728939bc53d43
Jason Santiago <jasonsantiagobutler@gmail.com>
2019-11-25 18:02:59 -0300

    changed app.listen port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
   const configService = app.get(ConfigService)
-  await app.listen(configService.get("PORT"))
+  await app.listen(process.env.PORT || configService.get("PORT") || 3000)
 }
 bootstrap();
